553ee0dfb9906399614860f97439cb8a62ea9892
Guest User <student@epicodus.com>
2015-04-21 16:11:06 -0700

    Add initial files.

---
+++ spec/scrabble_spec.rb
@@ -0,0 +1,14 @@
+require('rspec')
+require('scrabble')
+
+describe ('String#scrabble') do
+it("fetches a scrabble score for an inputted letter") do
+expect(("A").scrabble()).to(eq(1))
+end
+end
+
+describe ('String#scrabble') do
+  it("turns the word into a total score") do
+    expect(("ID").scrabble()).to(eq(3))
+end
+end
